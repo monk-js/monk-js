2b95ed5960e8fdc6ffccbff597485df2f6ea74c7
sergey-zholobov <sergeyrembo@yandex.ru>
2024-12-18 15:20:38 +0300

    Initial commit

---
+++ usr/src/runner.ts
@@ -0,0 +1,23 @@
+import * as path from 'path';
+import {getFileConstants, getFileList, runPackageCommand, getProcessArgs} from "@/utils";
+
+type BuildEnv = {
+    root: string,
+    packages: string
+}
+
+const {env, args} = getProcessArgs<BuildEnv>(process.argv);
+
+const command = args.shift();
+
+const {__root, __dirname} = getFileConstants(import.meta.url, env.root ?? process.cwd());
+
+env.packages ??= './packages';
+
+if (command != null) {
+    // Filtered list of `package.json` files from a specified directory.
+    const packages: string[] = getFileList(path.resolve(__root, env.packages), __dirname)
+        .filter((file: string) => !path.dirname(file).includes('node_modules') && path.basename(file) === 'package.json');
+
+    await runPackageCommand(packages, command as string, args as string[]);
+}
